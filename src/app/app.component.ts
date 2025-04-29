cf24ed5e0bbb59ac8e4c5d8c55b419c7c8cab822
juanortega <dannyortega0627@gmail.com>
2025-04-29 17:53:47 -0500

    Subo fronted a mi repositorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Importa el servicio Router
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
   title = 'login_detect';
+
+  constructor(private router: Router) {} // Inyecta el servicio Router
+
+  navigateToRegister() {
+    this.router.navigate(['/register']); // Navega a la ruta de registro
+  }
 }
---
+++ src/app/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  // Puedes dejarlo vacío por ahora
+}
