e16943337b6730131269f511eea5f14c74d3851f
juanortega <dannyortega0627@gmail.com>
2025-05-30 15:03:04 -0500

    Se añaden estilos a lista de dispositivos y estilos a ventanas emergentes

--- src/app/dashboard/components/livevideo/livevideo/livevideo.component.ts
+++ src/app/dashboard/components/livevideo/livevideo/livevideo.component.ts
@@ -1,16 +1,18 @@
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser, CommonModule } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-livevideo',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './livevideo.component.html',
   styleUrl: './livevideo.component.css'
 })
 export class LivevideoComponent {
   videoUrl: string = 'http://localhost:5000/video';
-  showVideo: boolean = false; // <-- Nueva variable
+  showVideo: boolean = false;
   devices: any[] = [];
 
   constructor(
@@ -52,19 +54,31 @@ export class LivevideoComponent {
   }
 
   activarDispositivo(device: any) {
-  const body = { nombre_camara: device.name };
-  this.http.post('http://localhost:5001/start', body).subscribe({
-    next: (response) => {
-      alert(`Cámara "${device.name}" activada correctamente`);
-      // Espera 2 segundos antes de mostrar el video
-      setTimeout(() => {
-        this.showVideo = true;
-      }, 2000);
-    },
-    error: (error) => {
-      alert('Error al activar la cámara');
-      console.error(error);
-    }
-  });
-}
+    const body = { nombre_camara: device.name };
+    this.http.post('http://localhost:5001/start', body).subscribe({
+      next: (response) => {
+        Swal.fire({
+          icon: 'success',
+          title: `Cámara "${device.name}" activada correctamente`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+        setTimeout(() => {
+          this.showVideo = true;
+        }, 2000);
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al activar la cámara',
+          text: 'Por favor verifica que el dispositivo esté disponible o conectado.',
+          confirmButtonText: 'Aceptar',
+          confirmButtonColor: '#218838'
+        
+
+        });
+        console.error(error);
+      }
+    });
+  }
 }
